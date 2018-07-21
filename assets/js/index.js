0f68df49a5dcc56ed106da16fb6141e402e1bd8d
marcusbrunee <marcusbrunee@gmail.com>
2018-07-21 19:33:21 -0300

    Corebiz teste

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	for(var i = 1; i <= 6; i++){
+		var img = '<img src="assets/img/anuncio.png"  style="margin-right:30px;cursor:pointer;">';
+		$("#anuncio-instragram").append(img);
+	}
+
+	for(var x = 1; x <=2; x++) {
+		var destaques = '<img src="assets/img/coluna_1.png" style="cursor: pointer;">';
+		$('.destaques').append(destaques);
+	}
+
+	/* Validação Newletter */
+	$("#newsletter").validate({
+		errorLabelContainer: "#error_msg",
+		wrapper: "div",
+		rules: {
+			newsletter_inp: {
+				required: true
+			}
+		},
+		messages: {
+			newsletter_inp: "Infome seu E-mail"
+		}
+	})
+})
